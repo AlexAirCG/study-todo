6038ddcbf23e067beebf6ad7ec3d3234ea594083
AlexAirCG <alexairggg@yandex.ru>
2024-08-06 12:08:18 +0300

    12:08/fix script

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/header";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between bg-[#E4E5E7] min-h-screen p-8">
+      <Script src="./main.js"></Script>
       <Header />
     </main>
   );
