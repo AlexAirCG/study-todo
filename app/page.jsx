e1cae39fa08152d47d9ead4c865ba1d8d243a40e
AlexAirCG <alexairggg@yandex.ru>
2024-08-07 11:22:50 +0300

    11:22 fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,9 @@
 import Header from "./components/header";
 import Script from "next/script";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between bg-[#E4E5E7] min-h-screen p-8">
-      <div>
-        <h1>Home</h1>
-        <Link href="/about">About</Link>
-      </div>
       <Script src="./main.js"></Script>
       <Header />
     </main>
